19762363169a006f798f3c3f38ecc33d66bdc6f2
Настенька <nastenka@192.168.1.5>
2022-08-03 21:01:24 +0800

    4 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+  
+  return friends.reduce((ul, { firstName, lastName }) => {
+    const li = document.createElement('li');
+    li.textContent = `${firstName} ${lastName}`;
+    ul.appendChild(li);
+    
+    return ul;
+  }, ul);
 }
